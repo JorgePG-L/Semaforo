b5fb1b51411eb29d5d63030771bfb46f1f4b3d0c
Jorge Piqueras García-Ligero <jorgepiqueras92@gmail.com>
2023-03-13 10:11:48 +0000

    Feat: First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+
+
+const [brillo, setBrillo ] = useState({
+	red:'',
+	yellow:'',
+	green:''
+});
+/*useEffect(() =>{
+	setBrillo({red:'selected',yellow:'',green:''})
+},[])*/
+const [counter, setCounter] = useState(0);
+const sumaCounter = () => {
+    if (counter === 0) {
+      setBrillo({ red: "selected", yellow: "", green: "" });
+    } else if (counter === 1) {
+      setBrillo({ red: "", yellow: "selected", green: "" });
+    } else if (counter === 2) {
+      setBrillo({ red: "", yellow: "", green: "selected" });
+    }
+
+    setCounter(prevCounter => {
+      const newCounter = prevCounter + 1;
+
+      if (newCounter >= 3) {
+        return 0;
+      }
+
+      return newCounter;
+    });
+  };
+
+	return ( 
+	
+	<div className="semaforo" >
+		<div className="back">
+		<div onClick={()=> setBrillo({red:'selected',yellow:'',green:''})} className={`red  ${brillo.red}`} ></div>
+		<div onClick={()=> setBrillo({red:'',yellow:'selected',green:''})} className={`yellow ${brillo.yellow}`}></div>
+		<div onClick={()=> setBrillo({red:'',yellow:'',green:'selected'})} className={`green ${brillo.green}`}></div>
+		
 		</div>
+		<button onClick={sumaCounter}>Click</button>
+	</div>
+		
+		
 	);
 };
 
